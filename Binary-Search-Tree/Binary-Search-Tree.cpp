ab84c8c8dc770f42879479bb8b3e4f7bdc331a03
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-06-13 18:24:50 +0700

    Add project files.

---
+++ Binary-Search-Tree/Binary-Search-Tree.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+#include <string> 
+using namespace std;
+class Node
+{
+public:
+	string info;
+	Node* leftchild;
+	Node* rightchild;
+	// Constructor for the node class Node(string i, Node* 1, Node* r)
+	{
+		info = i;
+		leftchild = 1;
+		right child = r;
+	}
+};
+
+class BinaryTree
+{
+public:
+	Node* ROOT;
+	Binary Tree()
+	{
+		ROOT = NULL; // Initializing ROOT to null
+	}
+	void insert(string element) // Insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, NULL, NULL); // Allocate memory for the new node newNode->info = element; // Assign value to the data field of the new node newNode->left child = NULL; // Make the left child of the new node point to NULL newNode->rightchild NULL; // Make the right child of the new node point to NULL
+		Node* parent = NULL;
+		=
